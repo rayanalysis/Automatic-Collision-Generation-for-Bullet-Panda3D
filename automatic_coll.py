ad464c0c8df9a76d1ac5c0060ea45c5ca0559cdc
rayanalysis <ray.analysis@gmail.com>
2021-04-17 17:35:55 -0600

    Update automatic_coll.py

--- automatic_coll.py
+++ automatic_coll.py
@@ -1,7 +1,7 @@
 '''
 BSD 3-Clause License
 
-Copyright (c) 2019, Logan Bier.
+Copyright (c) 2021, Logan Bier.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
